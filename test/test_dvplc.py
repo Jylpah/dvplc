720729c0fa3e5f4ce8b4687ef0a06caefe9f48c8
jylpah <jylpah@gmail.com>
2023-04-29 01:08:53 +0300

    FIX imports

--- test/test_dvplc.py
+++ test/test_dvplc.py
@@ -1,5 +1,7 @@
 from asyncio.log import logger
-import pytest, os, sys
+import pytest # type: ignore
+import os
+import sys
 
 from pathlib import Path # if you haven't already done so
 file = Path(__file__).resolve()
